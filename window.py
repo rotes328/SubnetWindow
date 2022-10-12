c217d5f587e2931926429017b4f81e207a83f12d
Michael Rotella <michaelrotella@Michaels-MacBook-Air.local>
2022-10-12 11:58:26 -0400

    Cleaned up code

--- window.py
+++ window.py
@@ -1,102 +1,122 @@
-import tkinter
 from tkinter import *
 from subnet import *
-from functools import partial
+
+def clear_output():
+    output1.config(text="")
+    output2.config(text="")
+    output3.config(text="")
+
+def clear_output_params():
+    network_output.configure(text="")
+    broadcast_output.configure(text="")
+    hostrange_output.configure(text="")
+
+def output(values):
+    supernet = values[5]
+    network_address_out = "{}/{}".format(values[0], values[1])
+    hostrange = "{}  -  {}".format(values[3], values[4])
+    if supernet is False:
+        output1.config(text="Network Address")
+        output2.config(text="Broadcast Address")
+        output3.config(text="Host Range")
+        clear_output_params()
+        network_output.configure(text=network_address_out)
+        broadcast_output.configure(text=values[2])
+        hostrange_output.configure(text=hostrange)
+    else:
+        output1.config(text="CIDR Range")
+        clear_output_params()
+        network_output.configure(text="")
+        broadcast_output.configure(text="")
+        hostrange_output.configure(text="")
+        network_output.configure(text=hostrange)
+
+
+def error_status(text):
+    status_bar.configure(text=text, bg="red", fg="white", font="Helvetica 14 bold")
+    network_output.configure(text="----------")
+    broadcast_output.configure(text="----------")
+    hostrange_output.configure(text="----------")
+
+def error_message(code):
+    if code == 1:
+        text = "Invalid IPv4 Address or Mask"
+    if code == 2:
+        text = "Invalid IPv4 Address"
+    elif code == 3:
+        text = "Invalid Mask"
+    elif code == 4:
+        text = "Mask cannot be /0."
+    elif code == 5:
+        text = "Mask cannot be longer than 32 bits."
+    elif code == 6:
+        text = "IP cannot start with 0"
+    elif code == 7:
+        text = "Multicast IP; not a subnet."
+    error_status(text)
 
 def calculate_subnet():
-    # line1 = label1
-    # line2 = label2
-    # line3 = label3
-    # print(line1)
     try:
         ip = ip_input.get()
         mask = subnet_input.get()
-
         mask = check_mask_format(mask)
         # Calculate the values info
-        # subnet_calc(ip, str(mask))
         values = subnet_calc(ip, mask)
-        if values is True:
-            status_bar.configure(text="Error", bg="red", fg="white", font="Helvetica 14 bold")
-            network_output.configure(text="----------")
-            broadcast_output.configure(text="----------")
-            hostrange_output.configure(text="----------")
-            # messagebox.showerror("Python Error", "Error!")
+        if isinstance(values,int):
+            error_message(values)
         else:
-            network_address_out = "{}/{}".format(values[0],values[1])
-            hostrange = "{}  -  {}".format(values[3], values[4])
-            supernet = values[5]
-            output1.config(text="")
-            output2.config(text="")
-            output3.config(text="")
-            status_bar.configure(text="", bg="#F0F0F0")
-            if supernet is False:
-                output1.config(text="Network Address")
-                output2.config(text="Broadcast Address")
-                output3.config(text="Host Range")
-                # Label(win, text="Network Address").grid(row=2)
-                # Label(win, text="Broadcast Address").grid(row=3)
-                # Label(win, text="Host Range").grid(row=4)
-                network_output.configure(text="")
-                broadcast_output.configure(text="")
-                hostrange_output.configure(text="")
-                network_output.configure(text=network_address_out)
-                broadcast_output.configure(text=values[2])
-                hostrange_output.configure(text=hostrange)
-                # print(f"The network address is: {values[0]}/{values[1]}.")
-                # print(f"The broadcast address is {values[2]}.")
-                # print(f"The host range is {values[3]} - {values[4]}")
-            else:
-                output1.config(text="CIDR Range")
-                # output2.config(text="")
-                # output3.config(text="")
-                # Label(win, text="CIDR Range").grid(row=2)
-                # Label(win, text="").grid(row=3)
-                # Label(win, text="").grid(row=4)
-                network_output.configure(text="")
-                broadcast_output.configure(text="")
-                hostrange_output.configure(text="")
-                network_output.configure(text=hostrange)
-                # broadcast_output.configure(text=values[2])
-                # hostrange_output.configure(text=hostrange)
-                # print(f"The CIDR range is {values[3]} - {values[4]}")
+            clear_output()
+            output(values)
+            status_bar.configure(text="© Mike Rotella 2022", fg="black", bg="#F0F0F0", font="Helvetica 14")
     except Exception as e:
-        status_bar.configure(text="Error", bg="red", fg="white", font="Helvetica 14 bold")
-        network_output.configure(text="----------")
-        broadcast_output.configure(text="----------")
-        hostrange_output.configure(text="----------")
+        error_message(1)
 
 
+# Create Window
 win = Tk()
 win.title("Subnet Calculator")
-ip_label = Label(win, text='IP Address')
-mask_label = Label(win, text='Mask')
+win.geometry("320x180")
+win.grid_rowconfigure(0, weight=1)
+win.grid_columnconfigure(0, weight=1)
+
+# Create labels
+ip_label = Label(win, text="IP Address")
+mask_label = Label(win, text="Mask")
 output1 = Label(win, text="Network Address")
 output2 = Label(win, text="Broadcast Address")
 output3 = Label(win, text="Host Range")
+network_output = Label(win, text="----------", font=("Courier 12 bold"))
+broadcast_output = Label(win, text="----------", font=("Courier 12 bold"))
+hostrange_output = Label(win, text="----------", font=("Courier 12 bold"))
+status_bar = Label(win, text="© Mike Rotella 2022", fg="black", relief=SUNKEN, bg="#F0F0F0", font="Helvetica 14")
+
+# Create input fields
+ip_input = Entry(win)
+subnet_input = Entry(win)
+
+# Organize grid
 ip_label.grid(row=0)
 mask_label.grid(row=1)
 output1.grid(row=2)
 output2.grid(row=3)
 output3.grid(row=4)
-status_bar = Label(win, text="", relief=SUNKEN, bg="#F0F0F0")
 status_bar.grid(row=6, column=0, columnspan=2, sticky = W+E)
-# Label(win, text="").grid(row=5)
-
-ip_input = Entry(win)
-subnet_input = Entry(win)
-network_output = Label(win, text="----------", font=("Courier 12 bold"))
-broadcast_output = Label(win, text="----------", font=("Courier 12 bold"))
-hostrange_output = Label(win, text="----------", font=("Courier 12 bold"))
-
 ip_input.grid(row=0, column=1)
 subnet_input.grid(row=1, column=1)
 network_output.grid(row=2, column=1)
 broadcast_output.grid(row=3, column=1)
 hostrange_output.grid(row=4, column=1)
 
+# Create buttons
+exit = Button(win, text="Exit", command=win.quit)
+exit.grid(row=5, column=0, sticky=W+E)
+calculate = Button(win, text="Calculate", command=calculate_subnet)
+calculate.grid(row=5, column=1, sticky=W+E, pady=4)
 
-Button(win, text='Exit', command=win.quit).grid(row=5, column=0, sticky=W, pady=4)
-Button(win, text='Calculate', command=calculate_subnet).grid(row=5, column=1, sticky=W, pady=4)
 
-win.mainloop()
+def main():
+    win.mainloop()
+
+
+if __name__ == "__main__":
+    main()
