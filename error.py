0b17c7bc357089e5b1d0ebe316c2d2f58e67eabb
Michael Rotella <michaelrotella@Michaels-MacBook-Air.local>
2022-10-11 20:02:22 -0400

    Modified

--- error.py
+++ error.py
@@ -3,17 +3,17 @@
 def error(code):
     # Used to flag errors
     if code == 1:
-        print("Invalid IPv4 address or mask.")
+        errorcode = "Invalid IPv4 address or mask."
     elif code == 2:
-        print("Invalid IPv4 address.")
+        errorcode = "Invalid IPv4 address."
     elif code == 3:
-        print("Invalid mask.")
+        errorcode = "Invalid mask."
     elif code == 4:
-        print("Mask cannot be /0.")
+        errorcode = "Mask cannot be /0."
     elif code == 5:
-        print("Mask cannot be longer than 32 bits.")
+        errorcode = "Mask cannot be longer than 32 bits."
     elif code == 6:
-        print("IP cannot start with 0.")
+        errorcode = "IP cannot start with 0."
     elif code == 7:
-        print("Multicast IP; not a subnet.")
-    quit(-1)
+        errorcode = "Multicast IP; not a subnet."
+    return errorcode
