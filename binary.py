c9f5eac5c2eb59df673f9083de1bef672399e038
Michael Rotella <michaelrotella@Michaels-MacBook-Air.local>
2022-10-11 22:00:41 -0400

    Updated error handling

--- binary.py
+++ binary.py
@@ -1,6 +1,22 @@
 # Mike Rotella 2022
-
-from error import error
+from tkinter import messagebox
+def error(code):
+    # Used to flag errors
+    if code == 1:
+        errorcode = "Invalid IPv4 address or mask."
+    elif code == 2:
+        errorcode = "Invalid IPv4 address."
+    elif code == 3:
+        errorcode = "Invalid mask."
+    elif code == 4:
+        errorcode = "Mask cannot be /0."
+    elif code == 5:
+        errorcode = "Mask cannot be longer than 32 bits."
+    elif code == 6:
+        errorcode = "IP cannot start with 0."
+    elif code == 7:
+        errorcode = "Multicast IP; not a subnet."
+    return errorcode
 
 class DottedDecimalString:
     def __init__(self, dotted_decimal_string):
@@ -18,25 +34,37 @@ class DottedDecimalString:
         :return: Valid IP as dotted decimal list
         """
         octets = self.dotted_decimal_string.split(".")
-        # Error if list doesn"t have 4 elements
+        # Error if list doesn't have 4 elements
         if len(octets) != 4:
-            error(2)
-        try:
-            octetList = []
-            for octet in octets:
-                # Error if IPv4 invalid or not number
-                if int(octet) not in range(0, 256):
-                    error(2)
-                else:
-                    octetList.append(octet)
-            if int(octetList[0]) == 0:
-                error(6)
-            elif int(octetList[0]) > 256:
-                error(2)
-            elif int(octetList[0]) > 223:
-                error(7)
-        except ValueError:
-            error(2)
+            mistake = True
+            return mistake
+        # try:
+        octetList = []
+        for octet in octets:
+        # Error if IPv4 invalid or not number
+            if int(octet) not in range(0, 256):
+                mistake = True
+                return mistake
+            else:
+                octetList.append(octet)
+        if int(octetList[0]) == 0:
+                mistake = True
+                return mistake
+                # error_text = error(6)
+                # messagebox.showerror("Python Error", error_text)
+        elif int(octetList[0]) > 256:
+                mistake = True
+                return mistake
+                # error_text = error(2)
+                # messagebox.showerror("Python Error", error_text)
+        elif int(octetList[0]) > 223:
+                mistake = True
+                return mistake
+                # error_text = error(7)
+                # messagebox.showerror("Python Error", error_text)
+        # except ValueError:
+            # error_text = error(2)
+            # messagebox.showerror("Python Error", error_text)
         return octetList
 
     def convert_mask_to_dotted_decimal_list(self):
@@ -46,8 +74,12 @@ class DottedDecimalString:
         octets = self.dotted_decimal_string.split(".")
         # Error without 4 elements
         if len(octets) != 4:
-            error(3)
-        return octets
+            mistake = True
+            return mistake
+            # error_text = error(3)
+            # messagebox.showerror("Python Error", error_text)
+        else:
+            return octets
 
 
 class BinaryList:
@@ -116,13 +148,13 @@ class DottedDecimalList:
         """
         byte_list = []
         byte_list = ByteList(byte_list)
-        try:
-            for byte in self.dotted_decimal_list:
-                clean_byte = BinaryString(str(bin(int(byte)))).remove_0b()
-                clean_byte_padded = BinaryString(clean_byte).pad(8)
-                byte_list.push(str(clean_byte_padded))
-        except ValueError:
-            error(1)
+        for byte in self.dotted_decimal_list:
+            clean_byte = BinaryString(str(bin(int(byte)))).remove_0b()
+            clean_byte_padded = BinaryString(clean_byte).pad(8)
+            byte_list.push(str(clean_byte_padded))
+        # except ValueError:
+            # error_text = error(1)
+            # messagebox.showerror("Python Error", error_text)
         # Replace "0b"
         byte_list = byte_list.remove_0b()
         binary_str = ByteList(byte_list).convert_to_long_binary()
@@ -143,28 +175,31 @@ class DottedDecimalList:
         """
         binary = []
         byte_list = ByteList(binary)
-        try:
-            for byte in self.dotted_decimal_list:
-                # Convert to binary and append to binary list
-                # Store in iterable list
-                byte = BinaryString((bin(int(byte))))
-                byte_clean = BinaryString(byte).remove_0b()
-                byte_padded = BinaryString(byte_clean).pad(8)
-                byte_list.push(str(byte_padded))
-        except ValueError:
-            error(3)
+        for byte in self.dotted_decimal_list:
+            # Convert to binary and append to binary list
+            # Store in iterable list
+            byte = BinaryString((bin(int(byte))))
+            byte_clean = BinaryString(byte).remove_0b()
+            byte_padded = BinaryString(byte_clean).pad(8)
+            byte_list.push(str(byte_padded))
+        # except ValueError:
+            # error_text = error(3)
+            # messagebox.showerror("Python Error", error_text)
         # Convert to binary string
         binary_str = str(byte_list.convert_to_long_binary().remove_0b())
         # No /0 mask
         if binary_str[0] == "0":
-            error(4)
+            mistake = True
+            return mistake
         # Must be less than 32 bits
         if len(binary_str) > 32:
-            error(5)
+            mistake = True
+            return mistake
         # Check is mask is valid (no 1s after a 0)
         for i in range(1, len(binary_str)):
             if binary_str[i] > binary_str[i - 1]:
-                error(3)
+                mistake = True
+                return mistake
         # Convert to binary list to iterate
         binary_list_fixed_as_string = str(binary_str)
         binary_list_fixed = convert_to_list(binary_list_fixed_as_string)
@@ -200,10 +235,17 @@ class ByteList:
         """
         :return: Binary as string (e.g. "0b101011...")
         """
+        # try:
         long_binary = "".join(self.byte_list)
         long_binary = "0b{0}".format(long_binary)
         long_binary = BinaryString(long_binary)
         return long_binary
+        # except TypeError:
+            # error_text = error(1)
+            # messagebox.showerror("Python Error", error_text)
+        # except UnboundLocalError:
+            # error_text = error(1)
+            # messagebox.showerror("Python Error", error_text)
 
     def remove_0b(self):
         """
@@ -213,6 +255,9 @@ class ByteList:
             octet = self.byte_list[i]
             self.byte_list[i] = octet.replace("0b", "")
         return self.byte_list
+        # except IndexError:
+            # error_text = error(1)
+            # messagebox.showerror("Python Error", error_text)
 
 
 class DottedBinaryList:
@@ -261,11 +306,11 @@ class BitmaskString:
         :return: Mask as dotted decimal in string format
         """
         binary_mask = []
-        try:
-            for i in range(int(self.bitmask)):
-                binary_mask.append("1")
-        except ValueError:
-            error(3)
+        for i in range(int(self.bitmask)):
+            binary_mask.append("1")
+        # except ValueError:
+            # error_text = error(3)
+            # messagebox.showerror("Python Error", error_text)
         binary_mask = BinaryList(binary_mask)
         binary_mask = binary_mask.reverse_pad(32)
         dotted_binary = BinaryList(binary_mask).convert_list_to_dotted_binary()
