9080c283f9e5706719cba77859aa12a878c38b2d
Michael Rotella <michaelrotella@Michaels-MacBook-Air.local>
2022-10-19 19:36:32 -0400

    Added <return> functionality and fixed empty input bug

--- window.py
+++ window.py
@@ -3,11 +3,13 @@ from subnet import *
 
 
 def check_input(input1, input2):
-    # If input is too long, return error code
+    # If input is invalid length, return error code
     if len(input1) > 15:
         return 2
     elif len(input2) > 15:
         return 3
+    elif len(input1) == 0 and len(input2) == 0:
+        return 0
     else:
         return None
 
@@ -61,15 +63,20 @@ def output(values):
         network_output.configure(text=hostrange)
 
 
-def error_status(text):
-    paint_normal_window()
-    status_bar.configure(text=text, bg="red", fg="white", font="Helvetica 14 bold")
+def paint_blank_status():
     network_output.configure(text="----------")
     broadcast_output.configure(text="----------")
     hostrange_output.configure(text="----------")
 
 
+def error_status(text):
+    paint_normal_window()
+    status_bar.configure(text=text, bg="red", fg="white", font="Helvetica 14 bold")
+    paint_blank_status()
+
+
 def errno(code):
+    text=None
     match code:
         case 1:
             text = "Invalid IPv4 Address or Mask"
@@ -89,10 +96,14 @@ def errno(code):
 
 
 def calculate_subnet():
+    paint_normal_window()
+    paint_blank_status()
     ip = ip_input.get()
     mask = subnet_input.get()
     check = check_input(ip, mask)
-    if isinstance(check, int):
+    if check == 0:
+        return
+    elif isinstance(check, int):
         errno(check)
         return
     if len(mask) < 4:
@@ -121,6 +132,10 @@ def calculate_subnet():
     paint_normal_status()
 
 
+def run_app():
+    calculate_subnet()
+
+
 # Create Window
 win = Tk()
 win.title("Subnet Calculator")
@@ -134,9 +149,9 @@ mask_label = Label(win, text="Mask")
 output1 = Label(win, text="Network Address")
 output2 = Label(win, text="Broadcast Address")
 output3 = Label(win, text="Host Range")
-network_output = Label(win, text="----------", font=("Courier 12 bold"))
-broadcast_output = Label(win, text="----------", font=("Courier 12 bold"))
-hostrange_output = Label(win, text="----------", font=("Courier 12 bold"))
+network_output = Label(win, text="----------", font="Courier 12 bold")
+broadcast_output = Label(win, text="----------", font="Courier 12 bold")
+hostrange_output = Label(win, text="----------", font="Courier 12 bold")
 status_bar = Label(win, text="© Mike Rotella 2022", fg="black", relief=SUNKEN, bg="#F0F0F0", font="Helvetica 14")
 
 # Create input fields
@@ -156,9 +171,12 @@ network_output.grid(row=2, column=1)
 broadcast_output.grid(row=3, column=1)
 hostrange_output.grid(row=4, column=1)
 
+# Bind return to run the app
+win.bind('<Return>', lambda e: run_app())
+
 # Create buttons
-exit = Button(win, text="Exit", command=win.quit)
-exit.grid(row=5, column=0, sticky=W+E)
+exit_button = Button(win, text="Exit", command=win.quit)
+exit_button.grid(row=5, column=0, sticky=W+E)
 calculate = Button(win, text="Calculate", command=calculate_subnet)
 calculate.grid(row=5, column=1, sticky=W+E, pady=4)
 
